e0e2d54c65c59ffe395e9fae104c258a1d653adc
guikominami <contato.mudalab@gmail.com>
2025-02-12 07:24:14 +0100

    translations done

--- models/translation.js
+++ models/translation.js
@@ -1,30 +1,24 @@
 const Joi = require("joi");
-Joi.objectId = require('joi-objectid')(Joi)
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const { wordSchema } = require("./word");
 
 const translationSchema = new mongoose.Schema({
   words: [
     {
-      type: new mongoose.Schema({
-        word: {
-          type: String,
-          required: true,
-          minlength: 1,
-          maxlength: 100,
-        }
-      }),
-    }
-  ]
+      type: wordSchema,
+      required: true,
+    },
+  ],
 });
 
 const Translation = mongoose.model("Translation", translationSchema);
 
-function validateTranslation(translation){
+function validateTranslation(translation) {
   const schema = Joi.object({
-    words: Joi.array().required()
-  })
-  
+    words: Joi.array().required(),
+  });
+
   return schema.validate(translation);
 }
 
--- routes/translations.js
+++ routes/translations.js
@@ -3,24 +3,78 @@ const { Word } = require("../models/word");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
   const translations = await Translation.find().sort("_id");
   res.send(translations);
 });
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
-  const words = await Word.find({ _id: { $in: req.body.words  } }) 
-  if (!words) return res.status(400).send("Invalid words.");
-  
+
+  let wordList = [];
+  let wordSearch;
+
+  for (const word of req.body.words) {
+    wordSearch = await Word.findById(word);
+
+    if (!wordSearch)
+      return res
+        .status(404)
+        .send(`The words with the given Id ${word} was not found.`);
+
+    wordList.push(wordSearch);
+  }
+
   let translation = new Translation({
-    words: words
-  })
-  
+    words: wordList,
+  });
+
   translation = await translation.save();
   res.send(translation);
-})
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let wordList = [];
+  let wordSearch;
+
+  for (const word of req.body.words) {
+    wordSearch = await Word.findById(word);
+
+    if (!wordSearch)
+      return res
+        .status(404)
+        .send(`The words with the given Id ${word} was not found.`);
+
+    wordList.push(wordSearch);
+  }
+
+  const translation = await Translation.findByIdAndUpdate(
+    req.params.id,
+    {
+      words: wordList,
+    },
+    { new: true }
+  );
+
+  if (!translation)
+    return res
+      .status(400)
+      .send("The translation with the given Id was not found.");
+
+  res.send(translation);
+});
+
+router.delete("/:id", async (req, res) => {
+  const translation = await Translation.findByIdAndDelete(req.params.id);
+  if (!translation)
+    return res
+      .status(400)
+      .send("The translation with the given Id was not found.");
+  res.send(translation);
+});
 
 module.exports = router;
